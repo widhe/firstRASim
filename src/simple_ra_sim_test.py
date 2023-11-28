8d0bcae604284fe1bc2cd94ce5cce59d23059946
widhe <123471818+widhe@users.noreply.github.com>
2023-11-28 14:17:03 +0100

    Restoring test settings to normal operation

--- src/simple_ra_sim_test.py
+++ src/simple_ra_sim_test.py
@@ -14,7 +14,7 @@ antenna_placement_method = 'circle'
 #antenna_placement_method = 'random_uniform'
 num_antennas = 20
 antenna_area_side = 100
-sw_test_baselines = True
+sw_test_baselines = False
 
 if sw_test_baselines:
     test_baselines = []
@@ -26,8 +26,10 @@ else:
 
 
 fig = plt.figure(figsize=(10, 10))
-simple_ra_sim('big_dipper.jpg', num_antennas, antenna_placement_method, antenna_area_side, image_size=(11,11),
-              test_val=(1, 0), test_baselines=test_baselines, sw_plot=True)
+#simple_ra_sim('big_dipper.jpg', num_antennas, antenna_placement_method, antenna_area_side, image_size=(11,11),
+#              test_val=(1, 0), test_baselines=test_baselines, sw_plot=True)
+simple_ra_sim('big_dipper.jpg', num_antennas, antenna_placement_method, antenna_area_side, image_size=(101,101),
+              test_val=None, test_baselines=test_baselines, sw_plot=True)
 
 fig.suptitle(f'Num antennas = {num_antennas}, Antenna placement method = {antenna_placement_method}, area_size = {antenna_area_side} m')
 plt.subplots_adjust(wspace=0.4)
